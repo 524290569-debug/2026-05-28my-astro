3d7edcec8f7f5a126dc31d785a68af79835c98fe
524290569-debug <524290569@qq.com>
2026-06-09 14:36:57 +0800

    Update Astro custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://524290569-debug.github.io',
-  base: '/2026-05-28my-astro',
+  site: 'https://aftert.one',
 });
